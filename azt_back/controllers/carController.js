63e23bc1d6625c1f1bfd5b4c3c40ad1da6af52d9
muhammedgunaydin <muhammed-gunaydin@hotmail.com>
2025-01-30 18:49:01 +0300

    car crud added

--- azt_back/controllers/carController.js
+++ azt_back/controllers/carController.js
@@ -3,10 +3,61 @@ const Car = require('../models/Car')
 exports.createCar = async (req, res) => {
   try {
     const car = await Car.create(req.body)
-    res.status(201).send(`${car.brand}+${car.model} created successfully`)
+    res.status(201).send(`${car.brand} ${car.model} created successfully`)
   } catch (err) {
     res.status(400).json({
       status: 'failed to create car',
+      err,
+    })
+  }
+}
+
+exports.getAllCars = async (req, res) => {
+  try {
+    const car = await Car.find()
+    res.status(200).json({ car })
+  } catch (err) {
+    res.status(400).json({
+      status: 'failed to get all cars',
+      err,
+    })
+  }
+}
+
+exports.GetCarById = async (req, res) => {
+  try {
+    const car = await Car.findById({ _id: req.params.id })
+    res.status(200).json({ car })
+  } catch (err) {
+    res.status(400).json({
+      status: 'failed to get car by id',
+      err,
+    })
+  }
+}
+
+exports.updateCar = async (req, res) => {
+  try {
+    const car = await Car.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: req.body }
+    )
+    res.status(200).send('Car updated succesfully')
+  } catch (err) {
+    res.status(400).json({
+      status: 'failed to update car',
+      err,
+    })
+  }
+}
+
+exports.deleteCar = async (req, res) => {
+  try {
+    const car = await Car.findOneAndDelete({ _id: req.params.id })
+    res.status(200).send('Car deleted successfully')
+  } catch (err) {
+    res.status(400).json({
+      status: 'failed to delete car',
       err,
     })
   }
--- azt_back/routers/carRouter.js
+++ azt_back/routers/carRouter.js
@@ -4,6 +4,10 @@ const carController = require('../controllers/carController')
 const router = express.Router();
 
 router.route('/create').post(carController.createCar)
+router.route('/').get(carController.getAllCars)
+router.route('/:id').get(carController.GetCarById)
+router.route('/:id').patch(carController.updateCar)
+router.route('/:id').delete(carController.deleteCar)
 
 module.exports = router
 
