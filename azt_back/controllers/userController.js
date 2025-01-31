4d6c05362642279abfc64f5775dd597b366a0d1d
muhammedgunaydin <muhammed-gunaydin@hotmail.com>
2025-01-31 19:11:17 +0300

    user crud updated

--- azt_back/controllers/userController.js
+++ azt_back/controllers/userController.js
@@ -44,6 +44,18 @@ exports.loginUser = async (req, res) => {
   }
 }
 
+exports.getAllUsers = async (req, res) => {
+  try {
+    const user = await User.find()
+    res.status(200).json(user)
+  } catch (err) {
+    res.status(500).json({
+      status: 'Failed to get users',
+      err,
+    })
+  }
+}
+
 exports.getUser = async (req, res) => {
   try {
     const { id } = req.params
--- azt_back/routers/userRouter.js
+++ azt_back/routers/userRouter.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.route('/register').post(userController.createUser)
 router.route('/login').post(userController.loginUser)
+router.route('/').get(userController.getAllUsers)
 router.route('/:id').get(userController.getUser)
 router.route('/:id').delete(userController.deleteUser)
 
